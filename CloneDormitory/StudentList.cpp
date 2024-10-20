79783cd1eb1a3de740cc7afb5b240328c51de973
Сергей Егоренков <egorenkovs12@gmail.com>
2024-10-20 18:29:07 +0300

    go back

--- CloneDormitory/StudentList.cpp
+++ CloneDormitory/StudentList.cpp
@@ -1,30 +1,21 @@
 #include "StudentList.h"
 
-bool checkForOriginality(std::vector<Floor>&floors, const StudentResident & newStudent) {
+bool checkForOriginality(std::vector<Floor>& floors, const StudentResident& newStudent) {
     for (auto& floor : floors) {
-        if (floor.getBlocks().empty()) {
+        if (floor.getBlocks().empty())
             continue;
-        }
 
-        bool foundDuplicate = false;
         for (auto& block : floor.getBlocks()) {
             const auto& residents = block.getResidents();
+            if (residents.empty())
+                continue;
 
-            if (!residents.empty()) {
-                for (const auto& resident : residents) {
-                    if (resident == newStudent) {
-                        foundDuplicate = true; 
-                        break;
-                    }
-                }
-            }
-
-            if (foundDuplicate) {
-                return false;
+            for (const auto& resident : residents) {
+                if (resident == newStudent)
+                    return false;
             }
         }
     }
-
     return true;
 }
 
